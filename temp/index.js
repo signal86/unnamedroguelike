4dc35bd93b34a383f7acfdae85d71eb1625079ff
gabe738 <gabe@patrickwheeler.com>
2024-04-04 14:02:47 -0500

    no longer frame dependent

--- temp/index.js
+++ temp/index.js
@@ -4,8 +4,6 @@ const ctx = canvas.getContext("2d")
 canvas.width = 1200
 canvas.height = 800
 
-const FPS = 40
-
 const currentKeysPressed = {};
 
 const background = new Image();
@@ -26,33 +24,44 @@ const player = {
     }
 }
 
-function gameLoop(){
+const dampingFactor = 17.5;
+const moveSpeed = 115;
+const gravity = 2500;
+const jumpForce = 900;
 
-    gravity(player, 2)
+let lastFrame = performance.now();
+
+function gameLoop(){
+    const dt = (performance.now() - lastFrame) / 1000;
+    lastFrame = performance.now();
+
+    
+    player.pos.x += player.vel.x * dt
+    player.pos.y += player.vel.y * dt
+    
+    applyGravity(player, gravity, dt)
     playerMovement()
 
-    player.pos.x += player.vel.x
-    player.pos.y += player.vel.y
+    player.vel.x /= 1 + dampingFactor * dt;
 
-    player.vel.x /= 2
-
-    console.log(currentKeysPressed)
+    // console.log(currentKeysPressed)
 
     ctx.imageSmoothingEnabled = false
     ctx.drawImage(background, 0, 0, canvas.width, canvas.height)
 
     ctx.fillStyle = "blue"
     ctx.fillRect(player.pos.x, player.pos.y, player.hitbox.x, -1*player.hitbox.y)
+
+    requestAnimationFrame(gameLoop)
 }
 
-function isOnGround(pos){
-    if (pos.y >= canvas.height) return true
-    else return false
-}
 
-function gravity(entity, force){
-    if (!isOnGround(entity.pos)){
-        entity.vel.y += force
+const isOnGround = playerY => playerY >= canvas.height
+
+
+function applyGravity(entity, force, dt){
+    if (!isOnGround(entity.pos.y)){
+        entity.vel.y += force * dt
     } else {
         entity.vel.y = 0
         entity.pos.y = canvas.height
@@ -60,17 +69,18 @@ function gravity(entity, force){
 }
 
 function playerMovement(){
-    if (currentKeysPressed["a"] == true) player.vel.x -= 5
-    if (currentKeysPressed["d"] == true) player.vel.x += 5
+    if (currentKeysPressed["a"] == true) player.vel.x -= moveSpeed;
+    if (currentKeysPressed["d"] == true) player.vel.x += moveSpeed;
 }
 
-gameInterval = setInterval(gameLoop, 1000 / FPS)
+gameLoop();
 
 function onKeypress(event) {
-    if (event.key == "a" || event.key == "d") currentKeysPressed[event.key] = true;
-    if (event.key == "w" && isOnGround(player.pos)){
+    
+    if (event.key.toLowerCase() == "a" || event.key.toLowerCase() == "d" || event.key.toLowerCase() == "arrowleft" || event.key.toLowerCase() == "arrowright") currentKeysPressed[event.key.toLowerCase()] = true;
+    if ((event.key.toLowerCase() == "arrowup" || event.key.toLowerCase() == "w" || event.key.toLowerCase() == " ") && isOnGround(player.pos.y)){
         player.pos.y -= 1
-        player.vel.y -= 23
+        player.vel.y -= jumpForce
         console.log("jump")
     }
 }
