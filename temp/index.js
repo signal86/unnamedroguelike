e9c7481206af30d3ae892af97ea03c5f7c95df8c
signal86 <signal86@github.com>
2024-04-10 14:34:43 -0500

    camera logic?

--- temp/index.js
+++ temp/index.js
@@ -7,10 +7,10 @@ canvas.height = 800;
 const boxes = [];
 
 
-const currentKeysPressed = {};
+const keyMap = {};
 
 const background = new Image();
-background.src = "./Assets/space.jpg";
+background.src = "./Assets/testbackground.png";
 
 class WallCollision {
 
@@ -29,9 +29,13 @@ class WallCollision {
 
     }
 
+    collide(box, player) {
+        
+    }
+
     draw() {
 
-        ctx.fillStyle("red");
+        ctx.fillStyle = "red";
         ctx.fillRect(this.x, this.y, this.w, this.h);
 
     }
@@ -61,72 +65,94 @@ const jumpForce = 900;
 
 let lastFrame = performance.now();
 
+
+const isOnGround = playerY => playerY >= canvas.height;
+
+
+function applyGravity(entity, force, dt) {
+    if (!isOnGround(entity.pos.y + entity.hitbox.y)){
+        entity.vel.y += force * dt;
+    } else {
+        entity.vel.y = 0;
+        entity.pos.y = canvas.height - entity.hitbox.y;
+    }
+}
+
+function playerMovement() {
+    if (player.wallColliding)
+        console.log("uhoh!");
+
+    if (keyMap.a) player.vel.x -= moveSpeed;
+    if (keyMap.d) player.vel.x += moveSpeed;
+    let v = false;
+    for (const box of boxes) {
+        // console.log("box.x = " + box.x + "\nplayer = " + player.hitbox.x + player.vel.x);
+        if (
+            box.x <= player.hitbox.x + player.pos.x &&
+            box.x + box.w >= player.pos.x &&
+            box.y <= player.hitbox.y + player.pos.y &&
+            box.y + box.h >= player.pos.y
+        ) {
+            v = true;
+            player.wallColliding = true;
+        }
+    } if (!v) player.wallColliding = false;
+}
+
+window.addEventListener('keydown', event => {
+    keyMap[event.key.toLowerCase()] = true;
+    
+    // if ((event.key.toLowerCase() == "arrowup" || event.key.toLowerCase() == "w" || event.key.toLowerCase() == " ") && isOnGround(player.pos.y + player.hitbox.y)){
+    if (isOnGround(player.pos.y + player.hitbox.y)) {
+        switch (event.key.toLowerCase()) {
+            case "arrowup":
+            case "w":
+            case " ":
+                player.pos.y -= 1;
+                player.vel.y -= jumpForce;
+                console.log("jump");
+        }
+    }
+});
+
+window.addEventListener('keyup', event => { keyMap[event.key] = false; });
+
+
 function gameLoop() {
+
+    // ctx.fillRect(0, 0, canvas.width, canvas.height);
+
     const dt = (performance.now() - lastFrame) / 1000;
     lastFrame = performance.now();
-
+    
+    
+    ctx.translate(-1 * (player.vel.x * dt), 0);
+    
+    ctx.imageSmoothingEnabled = false;
+    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
     
     player.pos.x += player.vel.x * dt;
     player.pos.y += player.vel.y * dt;
     
     applyGravity(player, gravity, dt);
     playerMovement();
-
-    player.vel.x /= 1 + dampingFactor * dt;
-
-    // console.log(currentKeysPressed)
-
-    ctx.imageSmoothingEnabled = false;
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    ctx.fillStyle = "blue";
-    ctx.fillRect(player.pos.x, player.pos.y, player.hitbox.x, -1*player.hitbox.y);
-
-
-    // for (const box in boxes) box.draw();
-    requestAnimationFrame(gameLoop);
-}
-
-
-const isOnGround = playerY => playerY >= canvas.height;
-
-
-function applyGravity(entity, force, dt) {
-    if (!isOnGround(entity.pos.y)){
-        entity.vel.y += force * dt;
-    } else {
-        entity.vel.y = 0;
-        entity.pos.y = canvas.height;
-    }
-}
-
-function playerMovement() {
-    if (player.wallColliding) console.log("uhoh!");
-    if (currentKeysPressed["a"] == true) player.vel.x -= moveSpeed;
-    if (currentKeysPressed["d"] == true) player.vel.x += moveSpeed;
-    for (const box of boxes) {
-        
-    }
-}
-
-
-function onKeyPress(event) {
     
-    if (event.key.toLowerCase() == "a" || event.key.toLowerCase() == "d" || event.key.toLowerCase() == "arrowleft" || event.key.toLowerCase() == "arrowright") currentKeysPressed[event.key.toLowerCase()] = true;
-    if ((event.key.toLowerCase() == "arrowup" || event.key.toLowerCase() == "w" || event.key.toLowerCase() == " ") && isOnGround(player.pos.y)){
-        player.pos.y -= 1;
-        player.vel.y -= jumpForce;
-        console.log("jump");
-    }
+    player.vel.x /= 1 + dampingFactor * dt;
+    
+    // console.log(currentKeysPressed)
+    
+    
+    ctx.fillStyle = "blue";
+    ctx.fillRect(player.pos.x, player.pos.y, player.hitbox.x, player.hitbox.y);
+    
+    for (const box of boxes) box.draw();
+    requestAnimationFrame(gameLoop);
+
 }
 
-function onKeyUp(event) {
-    if (event.key == "a" || event.key == "d") currentKeysPressed[event.key] = false;
-}
 
-window.addEventListener('keydown', onKeyPress);
-window.addEventListener('keyup', onKeyUp);
-
-
-WallCollision.addWall(200, 200, 15, 15);
+// WallCollision.addWall(1500, 700, 15, 100);
+WallCollision.addWall(1000, 700, 15, 100);
+WallCollision.addWall(900, 700, 15, 100);
+WallCollision.addWall(800, 600, 15, 160);
 gameLoop();
