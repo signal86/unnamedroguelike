45377e14f408b2ad6b1751ee9e7544bb7662ffa3
N-magic <n8martin2025@gmail.com>
2024-07-08 14:56:12 -0500

    startedTilingBackgrounds

---
+++ tilingCheck/index.js
@@ -0,0 +1,15 @@
+const canvas = document.querySelector("#render_canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 640
+canvas.height = 360
+
+const background = new Image();
+background.src = "./prototypeBackground02.png"
+
+for (let x = 0; x < canvas.width; x += 40) {
+	for (let y = 0; y < canvas.height; y += 40) {
+		ctx.drawImage(background,(0+(40*(Math.floor(2*Math.random())))),(0+(40*(Math.floor(2*Math.random())))),40,40,x,y);
+	}
+}
+
